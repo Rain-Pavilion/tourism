080949324c61538fd98bde5e9b2c25a9209cb3ff
2287963892 <www.2287963892@qq.com>
2019-09-18 21:08:04 +0800

    down

--- app/tourism_client/src/view/appointment.vue
+++ app/tourism_client/src/view/appointment.vue
@@ -16,14 +16,20 @@
         <img src="img/appointment/3j.png" alt="">
       </div>
       <div>
-        <span>主题</span>
-        <img src="img/appointment/3j.png" alt="">
+        <span @click="isSelected(0)">主题</span>
+        <img :class="poticratege" src="img/appointment/3j.png" alt="">
       </div>
       <div>
-        <span>时间</span>
-        <img src="img/appointment/3j.png" alt="">
+        <span @click="isSelected(1)">时间</span>
+        <img :class="timeratege" src="img/appointment/3j.png"  alt="">
       </div>   
     </section>
+    <div class="down" :class="isshow">
+      <ul>
+        <li>全部</li>
+        <li v-for="(item,i) in list" :key="i">{{item}}</li>
+      </ul>
+    </div>
     <div>
       <AppointmentCard v-for="i in 10" :key="i"></AppointmentCard>
     </div>
@@ -34,10 +40,26 @@ import AppointmentCard from "../component/appointment/appointmentCard"
 export default {
   data(){
     return {
-
+      selected:false,
+      istime:false,
+      isshow:{show:false},
+      poticratege:{ratege:false},
+      timeratege:{ratege:false},
+      lists:[["一起去旅行","陪我看电影","走！去探店","过节需要仪式感","约个饭吧","看展才是正经事","其它活动","约你去看剧"],["本周末","一周内","一个月内","十月","十一月","十二月","十二月之后"]],
+      list:[]
     }
   },
-  components:{AppointmentCard}
+  components:{AppointmentCard},
+  methods:{
+    isSelected(i){
+      this.list = this.lists[i];
+      this.selected = !this.selected;
+      this.isshow.show = this.selected;
+      if(i==0){this.poticratege.ratege= this.selected;}
+      else{this.timeratege.ratege= this.selected}
+      console.log(this.selected)
+    }
+  }
 };
 </script>
 <style scoped>
@@ -70,6 +92,7 @@ header button {
 }
 section {
   border-bottom: .01rem solid #d8d8d8;
+  position: relative;
 }
 section>div {
   width: 33.33%;
@@ -88,12 +111,33 @@ section>div img {
   width: 1rem;
   transition: 0.33s all;
 }
-section>div:hover img {
+section>div img {
   display: inline-block;
   height: 0.8rem;
   margin-left:0.25rem; 
   margin-top:0rem; 
   width: 1rem;
-  transform: rotate(180deg)
+}
+.down {
+  position: absolute;
+  top: 9rem;
+  width: 100%;
+  height: 0;
+  overflow: scroll;
+  transition: all 0.33s;
+  background-color: rgba(256,256,256,0.92);
+}
+.show {
+  height: 26.8rem;
+}
+.ratege {
+  transform: rotate(180deg);
+}
+.down ul li {
+  text-align: center;
+  height: 3.8rem;
+  line-height: 3.8rem;
+  font-size: 1.2rem;
+  font-weight: 700;
 }
 </style>
