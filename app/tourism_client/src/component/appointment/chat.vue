90326c3a5b35b906a8983e60fec0c96c43d7e034
2287963892 <www.2287963892@qq.com>
2019-09-27 10:25:37 +0800

    chat

--- app/tourism_client/src/component/appointment/chat.vue
+++ app/tourism_client/src/component/appointment/chat.vue
@@ -4,7 +4,13 @@
       <router-link to="/appointment">
         <img src="img/appointment/left.png" alt />
       </router-link>
+      <span>俞灏明</span>
     </header>
+    <section>
+      <p class="warn">
+      <van-icon name="warning-o" />此Biu伴非穷游官方发起，穷游仅提供信息展示，不代表认可其真实性和安全性。请仔细甄别，谨防广告、诈骗、谨慎向陌生人转账汇款、保护个人隐私及财产安全。
+      </p>
+    </section>
   </div>
 </template>
 <script>
@@ -13,5 +19,23 @@ export default {
 }
 </script>
 <style scoped>
-
+  header {
+    height: 10%;
+    display: flex;
+    border-bottom: 1px solid #ddd;
+    box-shadow:0px 2px 5px #888888;
+  }
+  header span {
+    display: inline-block;
+    height: 1rem;
+    margin:1rem 1rem;
+   }
+   header img {
+     margin: .5rem .5rem;
+   }
+   section .warn {
+     padding: 1rem 1rem;
+     font-size: 14px;
+     font-weight: normal;
+   }
 </style>
